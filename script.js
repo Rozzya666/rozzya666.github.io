4ca17dd60b6c6073e3f53c6791790b95aac162e6
RozzichBoyShark <lastdays02@gmail.com>
2023-11-20 02:15:36 +0200

    fix h2

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ const renderPopupProductCard = (product) => {
             : "/images/no-image.jpg"
         }" alt="Image">
                 <div class="about-item flex flex-col gap-2">
-                    <h2 class="md:text-4xl font-black max-w-[620px] truncate">${product.title}</h2>
+                    <h2 style="width: 250px" class="md:text-4xl font-black max-w-[620px] truncate">${product.title}</h2>
                     <svg width="197" height="25" viewBox="0 0 197 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M12.3562 0L15.8569 7.53796L24.1077 8.53794L18.0204 14.1966L19.619 22.3526L12.3562 18.3119L5.09341 22.3526L6.69201 14.1966L0.604756 8.53794L8.85555 7.53796L12.3562 0Z"
